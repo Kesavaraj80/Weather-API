f7d979dd5df4d66f09b59ff646bb5f8ca1e337f9
Kesavaraj08 <mkesavaraj1998@gmail.com>
2021-09-02 20:53:27 +0530

    final commit

--- script.js
+++ script.js
@@ -1,20 +1,17 @@
-let wnextday = [];
-// console.log(url);
+
 async function weather(){
   const city = await document.querySelector(".city").value;
   const url = "https://goweather.herokuapp.com/weather/"+city;
+  console.log(url);
   const data = await fetch(url);
   const finaldata = await data.json();
   console.log(finaldata)
-  finaldata.forecast.forEach(function(nextday){
-    nextdaydata(nextday);
-  });
-  if(wnextday.length>3){
-    wnextday.splice(0,3);
-    // console.log(wnextday);
-  }
-  console.log(wnextday.temperature);
-}
-function nextdaydata(nextday){
-  wnextday.push(nextday);
+  const city_name = document.querySelector(".city_name");
+  city_name.innerText="Weather in "+city;
+  const wind = document.querySelector(".wind");
+  wind.innerText=finaldata.wind;
+  const temp = document.querySelector(".temp");
+  temp.innerText=finaldata.temperature;
+  const desc = document.querySelector(".desc");
+  desc.innerText=finaldata.description;  
 }
